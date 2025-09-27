32775c8841c36c84a57a2b69abb58bac2ad65859
hirodinn <hirebikila1@gmail.com>
2025-09-27 21:13:44 +0300

    added insetion sort

--- script.js
+++ script.js
@@ -71,3 +71,20 @@ async function selectionSort() {
   }
   updateArrayBars();
 }
+//insertion sort
+async function insertionSort() {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      updateArrayBars([j, j + 1]);
+      await sleep(delay);
+      j--;
+    }
+    array[j + 1] = key;
+    updateArrayBars([], i);
+    await sleep(delay);
+  }
+  updateArrayBars();
+}
